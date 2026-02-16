92b66427726d9302ca78390594f1ca217f84ff25
Jedrzej Kosinski <kosinkadink1@gmail.com>
2026-02-15 21:25:41 -0800

    Make standalone the default install

--- sources/index.js
+++ sources/index.js
@@ -4,6 +4,6 @@ const git = require("./git");
 const remote = require("./remote");
 const cloud = require("./cloud");
 
-const sources = [portable, standalone, git, cloud, remote];
+const sources = [standalone, portable, git, cloud, remote];
 
 module.exports = sources;
